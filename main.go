876a50512529d9f56190a9924da3669adf08afe4
Timothy C. Lam <geroginalam@hotmail.com>
2020-04-02 01:44:33 +0800

    1 sec prime number under 10 millions without circular

--- main.go
+++ main.go
@@ -12,19 +12,20 @@ var allPrimeNumbersInList = make([]int, 0)
 // var visitedNumbers = make(map[int]struct{})
 
 const (
-	million10   = 10000000
-	million1    = 1000000
-	thousand100 = 100000
-	thousand10  = 10000
-	thousand    = 1000
-	hundred     = 100
+	million100  = 100000000 // 664,579
+	million10   = 10000000  // 664,579
+	million1    = 1000000   // 78,498
+	thousand100 = 100000    // 9,592
+	thousand10  = 10000     // 1,229
+	thousand    = 1000      // 1,68
+	hundred     = 100       // 25 prime
 )
 
 func main() {
-	const maxNumber = million10
-	fmt.Println(time.Now().Format(time.Kitchen))
-	nrOfCircularPrimer := findNrOfCircularPrimeNumber(maxNumber)
-	fmt.Println(time.Now().Format(time.Kitchen))
+	const maxNumber = million100
+	fmt.Println(time.Now().Format(time.StampNano))
+	nrOfCircularPrimer := findNrOfCircularPrimeNumber1(maxNumber)
+	fmt.Println(time.Now().Format(time.StampNano))
 
 	fmt.Println("Number of prime number is: ", len(allPrimeNumbers))
 	fmt.Println("Number of circular prime number for max number:", maxNumber, " is ", nrOfCircularPrimer)
@@ -42,7 +43,7 @@ func findNrOfCircularPrimeNumber(input int) int {
 		if _, ok := allPrimeNumbers[i]; !ok && isPrimeNumber(i) {
 			allPrimeNumbers[i] = struct{}{}
 			allPrimeNumbersInList = append(allPrimeNumbersInList, i)
-			nrOfCircularPrime += countCircularPrime(i)
+			// nrOfCircularPrime += countCircularPrime(i)
 		}
 	}
 
@@ -80,8 +81,8 @@ func countCircularPrime(input int) int {
 }
 
 func isPrimeNumber(input int) bool {
-	inputStr := strconv.Itoa(input)
-	if len(inputStr) > 1 {
+	if input >= 10 {
+		inputStr := strconv.Itoa(input)
 		inputStrLastChar := inputStr[len(inputStr)-1:]
 
 		if inputStrLastChar != "1" &&
@@ -99,4 +100,51 @@ func isPrimeNumber(input int) bool {
 	}
 
 	return true
+}
+
+func findNrOfCircularPrimeNumber1(input int) int {
+	nrOfCircularPrime := 0
+	storeInt := make([]int, input+1)
+	allPrimeNumbers[2] = struct{}{}
+	for i := 3; i <= input; i += 2 {
+		if storeInt[i] == 0 {
+			allPrimeNumbers[i] = struct{}{}
+
+			for j := i * i; j <= input; j += i {
+				storeInt[j] = j
+			}
+		}
+	}
+
+	return nrOfCircularPrime
+}
+
+func countCircularPrime1(input int) int {
+	inputStr := strconv.Itoa(input)
+	nrOfCircularPrime := len(inputStr)
+
+	// Test all nrOfCombination
+	combinationStr := ""
+
+	for i, nrOfCombination := 0, nrOfCircularPrime-1; i < nrOfCombination; i++ {
+		// Create nrOfCombination
+		combinationStr = inputStr[1:] + inputStr[:1]
+		combinationInt, _ := strconv.Atoi(combinationStr)
+
+		if combinationInt == input {
+			nrOfCircularPrime--
+		}
+
+		// Check if its prime number
+		if _, ok := allPrimeNumbers[i]; !ok && combinationInt != input && !isPrimeNumber(combinationInt) {
+			return 0
+		}
+
+		if _, ok := allPrimeNumbers[i]; !ok {
+			allPrimeNumbers[combinationInt] = struct{}{}
+			allPrimeNumbersInList = append(allPrimeNumbersInList, combinationInt)
+		}
+	}
+
+	return nrOfCircularPrime
 }
