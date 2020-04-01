406a47e4ca78c73cf5f988d5449720b7faf485ed
Timothy C. Lam <geroginalam@hotmail.com>
2020-04-02 02:25:26 +0800

    circular prime with repeated check

--- main.go
+++ main.go
@@ -6,10 +6,9 @@ import (
 	"time"
 )
 
-var allPrimeNumbers = make(map[int]struct{})
-var allPrimeNumbersInList = make([]int, 0)
-
-// var visitedNumbers = make(map[int]struct{})
+var storeInt []bool
+var prime = make(map[int]struct{})
+var totalCircularPrime int
 
 const (
 	million100  = 100000000 // 664,579
@@ -22,129 +21,58 @@ const (
 )
 
 func main() {
-	const maxNumber = million100
+	const maxNumber = million10
 	fmt.Println(time.Now().Format(time.StampNano))
-	nrOfCircularPrimer := findNrOfCircularPrimeNumber1(maxNumber)
+	findNrOfCircularPrimeNumber1(maxNumber)
+	countCircularPrime1()
 	fmt.Println(time.Now().Format(time.StampNano))
 
-	fmt.Println("Number of prime number is: ", len(allPrimeNumbers))
-	fmt.Println("Number of circular prime number for max number:", maxNumber, " is ", nrOfCircularPrimer)
+	fmt.Println("Max number is: ", maxNumber)
+	fmt.Println("Number of prime number is: ", len(prime))
+	fmt.Println("Number of circular prime number is ", totalCircularPrime)
 }
 
-func findNrOfCircularPrimeNumber(input int) int {
-	nrOfCircularPrime := 0
+func findNrOfCircularPrimeNumber1(input int) {
+	storeInt = make([]bool, input+1)
 
-	// Add 2 to prime number
-	allPrimeNumbers[2] = struct{}{}
-	allPrimeNumbersInList = append(allPrimeNumbersInList, 2)
-	nrOfCircularPrime++
+	// Add 2
+	storeInt[2] = true
+	prime[2] = struct{}{}
 
-	for i := 3; i <= input; i = i + 2 {
-		if _, ok := allPrimeNumbers[i]; !ok && isPrimeNumber(i) {
-			allPrimeNumbers[i] = struct{}{}
-			allPrimeNumbersInList = append(allPrimeNumbersInList, i)
-			// nrOfCircularPrime += countCircularPrime(i)
-		}
-	}
-
-	return nrOfCircularPrime
-}
-
-func countCircularPrime(input int) int {
-	inputStr := strconv.Itoa(input)
-	nrOfCircularPrime := len(inputStr)
-
-	// Test all nrOfCombination
-	combinationStr := ""
-
-	for i, nrOfCombination := 0, nrOfCircularPrime-1; i < nrOfCombination; i++ {
-		// Create nrOfCombination
-		combinationStr = inputStr[1:] + inputStr[:1]
-		combinationInt, _ := strconv.Atoi(combinationStr)
-
-		if combinationInt == input {
-			nrOfCircularPrime--
-		}
-
-		// Check if its prime number
-		if _, ok := allPrimeNumbers[i]; !ok && combinationInt != input && !isPrimeNumber(combinationInt) {
-			return 0
-		}
-
-		if _, ok := allPrimeNumbers[i]; !ok {
-			allPrimeNumbers[combinationInt] = struct{}{}
-			allPrimeNumbersInList = append(allPrimeNumbersInList, combinationInt)
-		}
-	}
-
-	return nrOfCircularPrime
-}
-
-func isPrimeNumber(input int) bool {
-	if input >= 10 {
-		inputStr := strconv.Itoa(input)
-		inputStrLastChar := inputStr[len(inputStr)-1:]
-
-		if inputStrLastChar != "1" &&
-			inputStrLastChar != "3" &&
-			inputStrLastChar != "7" &&
-			inputStrLastChar != "9" {
-			return false
-		}
-	}
-
-	for _, k := range allPrimeNumbersInList {
-		if input%k == 0 {
-			return false
-		}
-	}
-
-	return true
-}
-
-func findNrOfCircularPrimeNumber1(input int) int {
-	nrOfCircularPrime := 0
-	storeInt := make([]int, input+1)
-	allPrimeNumbers[2] = struct{}{}
 	for i := 3; i <= input; i += 2 {
-		if storeInt[i] == 0 {
-			allPrimeNumbers[i] = struct{}{}
+		if storeInt[i] == false {
+			prime[i] = struct{}{}
 
 			for j := i * i; j <= input; j += i {
-				storeInt[j] = j
+				storeInt[j] = true
 			}
 		}
 	}
-
-	return nrOfCircularPrime
 }
 
-func countCircularPrime1(input int) int {
-	inputStr := strconv.Itoa(input)
-	nrOfCircularPrime := len(inputStr)
+func countCircularPrime1() {
+	for input := range prime {
+		inputStr := strconv.Itoa(input)
+		nrOfCircularPrime := len(inputStr)
 
-	// Test all nrOfCombination
-	combinationStr := ""
+		for i, nrOfCombination := 0, nrOfCircularPrime-1; i < nrOfCombination; i++ {
+			// Create nrOfCombination
+			inputStr = inputStr[1:] + inputStr[:1]
+			combinationInt, _ := strconv.Atoi(inputStr)
 
-	for i, nrOfCombination := 0, nrOfCircularPrime-1; i < nrOfCombination; i++ {
-		// Create nrOfCombination
-		combinationStr = inputStr[1:] + inputStr[:1]
-		combinationInt, _ := strconv.Atoi(combinationStr)
+			if combinationInt == input {
+				nrOfCircularPrime--
+			}
 
-		if combinationInt == input {
-			nrOfCircularPrime--
+			// Check if its prime number
+			if _, ok := prime[combinationInt]; !ok {
+				nrOfCircularPrime = 0
+				break
+			}
 		}
 
-		// Check if its prime number
-		if _, ok := allPrimeNumbers[i]; !ok && combinationInt != input && !isPrimeNumber(combinationInt) {
-			return 0
-		}
-
-		if _, ok := allPrimeNumbers[i]; !ok {
-			allPrimeNumbers[combinationInt] = struct{}{}
-			allPrimeNumbersInList = append(allPrimeNumbersInList, combinationInt)
+		if nrOfCircularPrime != 0 {
+			totalCircularPrime++
 		}
 	}
-
-	return nrOfCircularPrime
 }
