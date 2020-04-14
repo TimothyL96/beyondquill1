545f2777e3e01a269c80286c0db0365efb646173
Timothy C. Lam <geroginalam@hotmail.com>
2020-04-14 18:32:48 +0800

    convert map to slice and switch back to int

--- main.go
+++ main.go
@@ -10,101 +10,113 @@ import (
 )
 
 var storeInt []bool
-var prime = make(map[uint32]struct{})
+var primeList []int
 var totalCircularPrime int
 
 const (
-	million100  = 100000000 // 5,761,455
-	million10   = 10000000  // 664,579
-	million1    = 1000000   // 78,498
-	thousand100 = 100000    // 9,592
-	thousand10  = 10000     // 1,229
-	thousand    = 1000      // 168
-	hundred     = 100       // 25 prime
+	million100  = 100000000 // 5,761,455 - 55
+	million10   = 10000000  // 664,579 - 55
+	million1    = 1000000   // 78,498 - 55
+	thousand100 = 100000    // 9,592 - 43
+	thousand10  = 10000     // 1,229 - 33
+	thousand    = 1000      // 168 - 25
+	hundred     = 100       // 25 prime circular 13
 	evenFilter  = "865420"
 )
 
 func main() {
+	// Set the input number size
 	const maxNumber = million10
-	var totalDuration time.Duration
 
+	// Start the timer
+	var totalDuration time.Duration
 	t := time.Now()
+
+	// Find all prime numbers and store in primeList
 	findPrimeNumbers(maxNumber)
+
+	// Find circular prime numbers from the found prime numbers
 	countCircularPrime()
+
+	// Stop the timer
 	totalDuration = time.Since(t)
 
-	fmt.Println("Total duration:", totalDuration)
+	// Print the result
+	fmt.Println("Total duration:", totalDuration.String())
 	fmt.Println("Max number is: ", maxNumber)
-	fmt.Println("Number of prime number is: ", len(prime))
+	fmt.Println("Number of prime number is: ", len(primeList))
 	fmt.Println("Number of circular prime number is ", totalCircularPrime)
 }
 
-func findPrimeNumbers(input uint32) {
+func findPrimeNumbers(input int) {
 	storeInt = make([]bool, input>>1)
 
 	// Add 2
-	storeInt[0] = true
-	prime[2] = struct{}{}
+	primeList = append(primeList, 2)
 
-	for i := uint32(3); i <= input; i += 2 {
+	for i := 3; i <= input; i += 2 {
 		if storeInt[i>>1] == false {
-			prime[i] = struct{}{}
+			primeList = append(primeList, i)
 
 			for j := i * i; j <= input; j += i << 1 {
 				storeInt[j>>1] = true
 			}
 		}
 	}
+
 }
 
 func countCircularPrime() {
-	circular := make(map[uint32]struct{})
+	// Create a map to quickly identify which prime number is checked
+	circular := make(map[int]struct{}, len(primeList))
 
-	for input := range prime {
-		_, ok := circular[input]
+	// Loop through all prime numbers
+	for i := len(primeList) - 1; i >= 0; i-- {
+		_, ok := circular[primeList[i]]
 
+		// Process only if this prime number is unprocessed (Not in the circular map)
 		if !ok {
-			inputStr := itoa(input)
-			if strings.ContainsAny(inputStr, evenFilter) && input != 2 && input != 5 {
+			inputStr := itoa(primeList[i])
+			if strings.ContainsAny(inputStr, evenFilter) && primeList[i] != 2 && primeList[i] != 5 {
 				continue
 			}
 
-			nrOfCircularPrime := len(inputStr)
-			circular[input] = struct{}{}
+			nrOfPossibleRotation := len(inputStr)
+			circular[primeList[i]] = struct{}{}
 
-			for i, nrOfCombination := 0, nrOfCircularPrime-1; i < nrOfCombination; i++ {
+			for j, nrOfCombination := 0, nrOfPossibleRotation-1; j < nrOfCombination; j++ {
 				// Create nrOfCombination
 				inputStr = inputStr[1:] + inputStr[:1]
 				combinationInt := atoi(inputStr)
 				circular[combinationInt] = struct{}{}
 
-				if combinationInt == input {
-					nrOfCircularPrime--
+				if combinationInt == primeList[i] {
+					nrOfPossibleRotation--
 				}
 
 				// Check if its prime number
-				if _, ok := prime[combinationInt]; !ok {
-					nrOfCircularPrime = 0
+				if storeInt[combinationInt>>1] {
+					nrOfPossibleRotation = 0
 					break
 				}
 			}
 
-			totalCircularPrime += nrOfCircularPrime
+			totalCircularPrime += nrOfPossibleRotation
 		}
 	}
 }
 
-func atoi(s string) uint32 {
+func atoi(s string) int {
 	// Fast path for small integers that fit int type.
-	var n uint32
+	var n int
 	for _, ch := range []byte(s) {
 		ch -= '0'
-		n = n*10 + uint32(ch)
+		n = n*10 + int(ch)
 	}
 
 	return n
 }
 
-func itoa(i uint32) string {
+func itoa(i int) string {
 	return strconv.FormatInt(int64(i), 10)
 }
